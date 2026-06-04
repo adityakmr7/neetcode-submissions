49f5467b306eb536ac043fe1782baa584bc61594
Aditya Kumar <adityakmr9672@gmail.com>
2026-06-04 17:41:19 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let maxProfit = 0;
+
+        for(let i = 0;i < prices.length;i++) {
+            for(let j = i +1;j < prices.length;j++) {
+                let buyPrice = prices[i];
+                let sellPrice = prices[j];
+                let profit = sellPrice - buyPrice;
+                maxProfit = Math.max(maxProfit,profit);
+            }
+        }
+        return maxProfit
+    }
+}
