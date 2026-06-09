47d2653ea52c0df40e30dc7f354909b5a26e53c3
Aditya Kumar <adityakmr9672@gmail.com>
2026-06-09 22:37:37 +0530

    Add: dynamicArray - submission-0

---
+++ Data Structures & Algorithms/dynamicArray/submission-0.js
@@ -0,0 +1,75 @@
+class DynamicArray {
+    /**
+     * @constructor
+     * @param {number} capacity
+     */
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.array = new Array(capacity);
+        this.size = 0;
+    }
+
+    /**
+     * @param {number} i
+     * @returns {number}
+     */
+    get(i) {
+        return this.array[i];
+    }
+
+    /**
+     * @param {number} i
+     * @param {number} n
+     * @returns {void}
+     */
+    set(i, n) {
+        this.array[i]= n;
+    }
+
+    /**
+     * @param {number} n
+     * @returns {void}
+     */
+    pushback(n) {
+        if(this.size === this.capacity) {
+            this.resize();
+        }
+        this.array[this.size] = n;
+        this.size++;
+
+    }
+
+    /**
+     * @returns {number}
+     */
+    popback() {
+        this.size--;
+        return this.array[this.size];
+    }
+
+    /**
+     * @returns {void}
+     */
+    resize() {
+        this.capacity  *= 2;
+        const newArray = new Array(this.capacity);
+        for(let i = 0 ; i < this.size;i++) {
+            newArray[i] = this.array[i];
+        }
+        this.array = newArray;
+    }
+
+    /**
+     * @returns {number}
+     */
+    getSize() {
+        return this.size
+    }
+
+    /**
+     * @returns {number}
+     */
+    getCapacity() {
+        return this.capacity;
+    }
+}
