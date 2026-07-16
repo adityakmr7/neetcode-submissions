91923899126e05b48a663ae9b62c641779c2c613
Aditya Kumar <adityakmr9672@gmail.com>
2026-07-16 09:29:08 +0530

    Add: binary-tree-diameter - submission-0

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-0.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    diameterOfBinaryTree(root) {
+        let maxDiameter = 0
+
+        function getDepth(root) {
+            if(root === null) return 0;
+            let leftDepth = getDepth(root.left);
+            let rightDepth = getDepth(root.right);
+            let diameter = leftDepth + rightDepth;
+            maxDiameter = Math.max(maxDiameter, diameter);
+            return Math.max(leftDepth,rightDepth) +1
+        }
+
+
+        getDepth(root);
+
+        return maxDiameter;
+    }
+}
